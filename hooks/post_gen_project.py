f94f4135401ff1af31efedbb5444a6c907483478
Ryuji TSUTSUI <ryu22e@gmail.com>
2022-06-15 14:18:09 +0900

    Fix to always remove sphinx_extensions in hooks/post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,16 @@
 import shutil
 from pathlib import Path
 
+sphinx_extensions_dir = Path("sphinx_extensions")
 if "{{ cookiecutter.enable_budoux }}" == "y":
     file_name = "sphinx_budoux.py"
     ext_dir = Path("source") / "_ext"
     ext_dir.mkdir()
     ext_path = ext_dir / file_name
-    source_dir = Path("sphinx_extensions")
-    source_path = source_dir / file_name
+    source_path = sphinx_extensions_dir / file_name
 
     shutil.copyfile(source_path, ext_path)
-    shutil.rmtree(source_dir)
+shutil.rmtree(sphinx_extensions_dir)
 
 if "{{ cookiecutter.enable_robots_txt }}" != "y":
     robots_txt_path = Path("source") / "robots.txt"
